45bf4c5adec2a870eb8f166ae28bf87efcc17349
Stefan Neagu <stefanneagu@MacBook-Air-Stefan.local>
2025-06-02 22:46:14 +0300

    feat: imagini locale adăugate pentru produse

--- src/data/products.js
+++ src/data/products.js
@@ -1,106 +1,109 @@
+// Laptopuri
+import lenovoImg from "../assets/laptop-lenovo.jpg";
+import asusImg from "../assets/laptop-asus.jpg";
+import dellImg from "../assets/laptop-dell.jpg";
+
+// PC-uri
+import gamingRyzenImg from "../assets/unitate-gaming.jpg";
+import officeIntelImg from "../assets/pc-office-intel.jpg";
+
+// Plăci
+import nvidiaImg from "../assets/placa-nvidia.jpg";
+import msiImg from "../assets/placa-msi.jpg";
+
+// Smartphone-uri
+import iphoneImg from "../assets/iphone14pro.jpg";
+import samsungImg from "../assets/galaxy-s23.jpg";
+import xiaomiImg from "../assets/redmi-note12.jpg";
+
+// Smartwatch-uri
+import appleWatchImg from "../assets/apple-watch-se.jpg";
+import samsungWatchImg from "../assets/galaxy-watch5.jpg";
+
 const products = [
-  // Laptopuri
   {
     id: 1,
     name: "Laptop Lenovo IdeaPad",
     price: 3499,
     category: "Laptop",
-    image:
-      "https://www.lenovo.com/medias/lenovo-laptop-ideapad-1-15-intel-hero.png?context=bWFzdGVyfHJvb3R8NzI2Nzl8aW1hZ2UvcG5nfGg5ZC9oZTQvMTExNTczNDQ1NjkxODIucG5nfDU0NDQwYzM4ZTYxZjMzNmM4ZGRhYTI3ZTRhZjc0YTVlNjUwYjM4ODJmYmY3NmMxMzg3ZTYyMjU3NTBlNjIwNTc",
+    image: lenovoImg,
   },
   {
     id: 2,
     name: "Laptop ASUS ROG",
     price: 6299,
     category: "Laptop",
-    image:
-      "https://dlcdnwebimgs.asus.com/gain/18FC4341-43E1-466F-81F6-4B3B17AF4F3F",
+    image: asusImg,
   },
   {
     id: 3,
     name: "Laptop Dell Inspiron",
     price: 4199,
     category: "Laptop",
-    image:
-      "https://i.dell.com/sites/csimages/Video_Imagery/all/inspiron-15-3501-laptop.jpg",
+    image: dellImg,
   },
-
-  // PC-uri
   {
     id: 4,
     name: "Unitate Gaming Ryzen 7",
     price: 5599,
     category: "PC",
-    image: "https://www.pcgarage.ro/img/produs/2021/11/4083604/4697077.jpg",
+    image: gamingRyzenImg,
   },
   {
     id: 5,
     name: "PC Office Intel i5",
     price: 2999,
     category: "PC",
-    image:
-      "https://www.magazinultau.ro/uploads/gallery/normal/pc-gaming-intel-core-i5-12400-16gb-ssd-1tb-rtx3060-12gb.jpg",
+    image: officeIntelImg,
   },
-
-  // Plăci
   {
     id: 6,
     name: "Placă video NVIDIA RTX 4060",
     price: 2399,
     category: "Placi",
-    image:
-      "https://media.ldlc.com/r1600/ld/products/00/06/10/56/LD0006105675_2.jpg",
+    image: nvidiaImg,
   },
   {
     id: 7,
     name: "Placă de bază MSI B550",
     price: 699,
     category: "Placi",
-    image: "https://www.msi.com/image/mb/B550M-PRO-VDH-WIFI-gallery-1.jpg",
+    image: msiImg,
   },
-
-  // Smartphone-uri
   {
     id: 8,
     name: "iPhone 14 Pro",
     price: 6999,
     category: "Smartphone",
-    image:
-      "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iPhone-14-Pro_5up-Purple_220907_inline.jpg.large.jpg",
+    image: iphoneImg,
   },
   {
     id: 9,
     name: "Samsung Galaxy S23",
     price: 6499,
     category: "Smartphone",
-    image:
-      "https://images.samsung.com/is/image/samsung/p6pim/levant/sm-s911bzkamea/gallery/levant-galaxy-s23-s911-sm-s911bzkamea-thumb-535258771?$160_160_PNG$",
+    image: samsungImg,
   },
   {
     id: 10,
     name: "Xiaomi Redmi Note 12",
     price: 1199,
     category: "Smartphone",
-    image:
-      "https://www.notebookcheck.net/uploads/tx_nbc2/XiaomiRedmiNote12__1__01.JPG",
+    image: xiaomiImg,
   },
-
-  // Ceasuri smart
   {
     id: 11,
     name: "Apple Watch SE",
     price: 1599,
     category: "Smartwatch",
-    image:
-      "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MYDP2_VW_PF+watch-40-alum-silver-nc-se_VW_PF_WF_CO_GEO_US?wid=940&hei=1112&fmt=png-alpha&.v=1599010666000",
+    image: appleWatchImg,
   },
   {
     id: 12,
     name: "Samsung Galaxy Watch 5",
     price: 1399,
     category: "Smartwatch",
-    image:
-      "https://images.samsung.com/is/image/samsung/p6pim/levant/sm-r910nzvamea/gallery/levant-galaxy-watch5-44mm-r910-sm-r910nzvamea-thumb-533726861?$130_130_PNG$",
+    image: samsungWatchImg,
   },
 ];
 
