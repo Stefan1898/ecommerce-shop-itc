e5335d57648c7726030fea787ff0daaca758c5ab
Stefan Neagu <stefanneagu@MacBook-Air-Stefan.local>
2025-06-02 20:06:16 +0300

    Final fix: Navbar corectat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
       <h1 className="text-xl font-bold">E-Commerce Shop (IT/C)</h1>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/produse" className="hover:underline">Produse</Link>
-        <Link to="/cos" className="hover:underline">Coș</Link>
+        <Link to="/">Home</Link>
+        <Link to="/products">Produse</Link>
+        <Link to="/cart">Coș</Link>
       </div>
     </nav>
   );
